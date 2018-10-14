9f285f82dad59ffe7253f96f8615fba007ff06ce
cwjung479 <35707942+cwjung479@users.noreply.github.com>
2018-10-14 16:37:52 -0700

    Add files via upload

--- I_Wanna_Be_The_Best/src/application/Main.java
+++ I_Wanna_Be_The_Best/src/application/Main.java
@@ -1,50 +1,35 @@
-package application;
-	
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.util.Duration;
+package IWannaBeTheBest;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.transform.Scale;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		String str = "Hello World!";
-		
-		Text text = new Text();
-        VBox root = new VBox(text);
-        root.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(root, 330, 120, Color.WHITE);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-        final IntegerProperty i = new SimpleIntegerProperty(0);
-        Timeline timeline = new Timeline();
-        KeyFrame keyFrame = new KeyFrame(
-                Duration.seconds(.1),
-                event -> {
-                    if (i.get() > str.length()) {
-                        timeline.stop();
-                    } else {
-                        text.setText(str.substring(0, i.get()));
-                        i.set(i.get() + 1);
-                    }
-                });
-        timeline.getKeyFrames().add(keyFrame);
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+    	Screen screen = Screen.getPrimary();
+    	Rectangle2D bounds = screen.getVisualBounds();
+    	MainMenu mainMenu = new MainMenu();
+
+
+    	Scene home = new Scene(mainMenu.mMenu(), 800, 600);
+
+    	primaryStage.setWidth(bounds.getHeight() / 1.5);
+    	primaryStage.setHeight(bounds.getHeight() / 1.5);
+
+    	primaryStage.setTitle("Relationships");
+    	primaryStage.setScene(home);
+    	primaryStage.show();
+    }
 }
