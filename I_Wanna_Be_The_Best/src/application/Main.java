f8c5e4eac34b5b64f6fa02f569f021148b79dfbf
danielcodin <daniel@danielcoding.com>
2018-10-18 20:07:13 -0700

    [Ticket: Transition], [Ticket: TypingTextEffect]
    1. New "TypingScene" Class offer as a texteffect scene superclass that can be extended from.
    
    2. Text showing in TypingScene automatically wrap.
    
    3. Call TypingScene.playText() multiple times to keep displaying next messages.
    
    4. In Main.java, transition between scenes is functioning.
    
    5. In Main.java, press space to play next message in TypingScene, and if TypingScene run out of messages, press space to switch to next TypingScene.

--- I_Wanna_Be_The_Best/src/application/Main.java
+++ I_Wanna_Be_The_Best/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -8,6 +9,7 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -17,19 +19,92 @@ import javafx.util.Duration;
 
 public class Main extends Application {
 	
+	// ScreenWidth and screenHeight
+	final private int screenWidth = 640, screenHeight = 360;
+	
+	// All TypingScenes
+	private TypingScene[] allTypingScenes;
+	
+	// Current TypingScene index
+	private int currentTypingSceneIndex = -1;
+	
 	// Scroll speed in seconds
 	private double scrollSpeed = 1;
 	
+	// Space key listener
+	private boolean spaceKeyPressed = false;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		// First scene
-		TypingScene typingScene = new TypingScene();
+		// Set allTypingScenes
+		setAllTypingScenes();
 		
-        Scene scene = new Scene(typingScene.init_scene(), 330, 120, Color.WHITE);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		// Show next typingScene
+        showNextTypingScene(primaryStage);
+	}
+	
+	private void listenToKey(Stage primaryStage, Scene scene) {
+		scene.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.SPACE) spaceKeyPressed = true;
+        });
+		
+        scene.setOnKeyReleased(event -> {
+        	if(event.getCode() == KeyCode.SPACE) spaceKeyPressed = false;
+        });
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if(spaceKeyPressed) {
+                    spaceKeyPressed = false; // So no longer trigger multiple times when only pressed once
+ 
+                    if(allTypingScenes[currentTypingSceneIndex].playText(scrollSpeed) == false) {
+                    	showNextTypingScene(primaryStage);
+                    }
+                }
+            }
+        }.start();
+	}
+	
+	private void setAllTypingScenes() {
+		// Initialize how many scenes
+		allTypingScenes = new TypingScene[2];
+		
+		// Initialize what scene to include and in what order
+		allTypingScenes[0] = new FirstScene();
+		allTypingScenes[1] = new SecondScene();
+	}
+	
+	private void showNextTypingScene(Stage primaryStage) {
+		// Check if run out of TypingScene
+		if(currentTypingSceneIndex+1 >= allTypingScenes.length) {
+			System.out.println("Warning: run out of TypingScene to play");
+			return;
+		}
+		
+		// Get current TypingScene
+		TypingScene thisTypingScene = allTypingScenes[++currentTypingSceneIndex];
+		
+		if(currentTypingSceneIndex == 0) {
+			// First time, init scene
+			
+			// Init scene
+			Scene scene = new Scene(thisTypingScene.init_scene(screenWidth, screenHeight), screenWidth, screenHeight, Color.WHITE);
+	        primaryStage.setScene(scene);
+	        
+	        // Listen to key
+	        listenToKey(primaryStage, scene);
+	        
+	        // Show stage
+	        primaryStage.show();
+		} else {
+			// Not first time, only change the root of the scene
+			primaryStage.getScene().setRoot(thisTypingScene.init_scene(screenWidth, screenHeight));
+		}
+		
         
-        typingScene.playText(scrollSpeed);
+        // Play scene text
+        if(thisTypingScene.playText(scrollSpeed) == false) showNextTypingScene(primaryStage);
 	}
 	
 	public static void main(String[] args) {
--- I_Wanna_Be_The_Best/src/application/TypingScene.java
+++ I_Wanna_Be_The_Best/src/application/TypingScene.java
@@ -8,47 +8,91 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
 
 public class TypingScene {
 
-	private Text text;
-	private String str;
+	private Text text = null;
+	protected String[] strArray = null;
+	
+	// Current string index
+	protected int strIndex = 0;
+	
+	// Play text timeline
+	protected Timeline textTimeline = null;
 	
 	public TypingScene() {
-		text = new Text();
-		
-		// Playing text content
-		str = "Hello World!";
-		
+		// Set strArray
+		setStrArray();
 	}
 	
-	public VBox init_scene(){
+	public VBox init_scene(int screenWidth, int screenHeight){
+		// Set Text
+		text = new Text();
+		text.setTextAlignment(TextAlignment.CENTER);
+		text.wrappingWidthProperty().set(screenWidth - 200 > 150 ? screenWidth - 200 : screenWidth);
+		
+		// Set VBox
         VBox root = new VBox(text);
         root.setAlignment(Pos.CENTER);
         
         return root;
 	}
 	
-	public void playText(double scrollSpeed) {
-		// Given each char 0.01 seconds to play initially,xw
-		// the larger the scrollSpeed is, the faster it plays
-		double animate_duration = 0.01*str.length()/scrollSpeed;
+	// Return false when running out of strings (possibly means should go to next scene)
+	public boolean playText(double scrollSpeed) {
+		// check if has previous string playing
+		if(textTimeline != null) {
+			// Show entire string immediately
+			text.setText(strArray[strIndex-1]);
+			
+			// Stop timeline
+			textTimeline.stop();
+			textTimeline = null;
+			
+			// Return so player can have time to look at the string
+			return true;
+		}
 		
+		// Check if run out of strings
+		if(strIndex >= strArray.length) {
+			System.out.println("Warning: run out of strings to play");
+			return false;
+		}
+		
+		// Go to the new string
+		String thisString = strArray[strIndex++];
+		
+		// Given each char 0.05 seconds to play by default,
+		// the larger the scrollSpeed is, the faster it plays
+		double animate_duration = 0.05/scrollSpeed;
+		
+		// Play string
 		final IntegerProperty i = new SimpleIntegerProperty(0);
-        Timeline timeline = new Timeline();
+		textTimeline = new Timeline();
         KeyFrame keyFrame = new KeyFrame(
                 Duration.seconds(animate_duration),
                 event -> {
-                    if (i.get() > str.length()) {
-                        timeline.stop();
+                    if (i.get() > thisString.length()) {
+                    	textTimeline.stop();
+                    	textTimeline = null;
                     } else {
-                        text.setText(str.substring(0, i.get()));
+                        text.setText(thisString.substring(0, i.get()));
                         i.set(i.get() + 1);
                     }
                 });
-        timeline.getKeyFrames().add(keyFrame);
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
+        textTimeline.getKeyFrames().add(keyFrame);
+        textTimeline.setCycleCount(Animation.INDEFINITE);
+        textTimeline.play();
+        
+        return true;
+	}
+	
+	protected void setStrArray() {
+		strArray = new String[5];
+		for(int i = 0; i < strArray.length; i++) {
+			strArray[i] = "Override setStrArray() to customize strings. Number: " + i;
+		}
 	}
 }
