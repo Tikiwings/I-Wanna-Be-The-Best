66df9bc0475cb03e56a77dfdddd4d29033223708
tikiwings <donaldsanchez7870@gmail.com>
2018-11-14 11:07:40 -0800

    pulled from master / rearanged files to work correctly

--- src/application/GameManager.java
+++ src/application/GameManager.java
@@ -3,8 +3,6 @@ package application;
 import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -16,10 +14,18 @@ public class GameManager {
 	private int screenWidth = 640, screenHeight = 360;
 	private double scrollSpeed = 1;
 	
-	private TypingScene currentTypingScene = null;
-		
+	private TypingScene[] allTypingScenes;
+	private int currentTypingSceneIndex = -1;
+	
+	// Space key listener
+	private boolean spaceKeyPressed = false;
+	
+	
 	public GameManager(Stage primaryStage) {
 		this.primaryStage = primaryStage;
+		
+		// Set allTypingScenes
+		setAllTypingScenes();
 	}
 	
 	public void setScreenSize(int screenWidth, int screenHeight) {
@@ -31,21 +37,38 @@ public class GameManager {
 		this.scrollSpeed = scrollSpeed;
 	}
 	
+	// Private Methods
+	private void setAllTypingScenes() {
+		// Initialize how many scenes
+		allTypingScenes = new TypingScene[2];
+		
+		// Initialize what scene to include and in what order
+		allTypingScenes[0] = new FirstScene();
+		allTypingScenes[1] = new SecondScene();
+	}
 	
 	public void startGame() {
 		// Show next (first) typingScene
-        showNextTypingScene(new FirstScene(this));
+        showNextTypingScene();
 	}
 	
-	public void showNextTypingScene(TypingScene typingScene) {
-		currentTypingScene = typingScene;
+	private void showNextTypingScene() {
+		// Check if run out of TypingScene
+		if(currentTypingSceneIndex+1 >= allTypingScenes.length) {
+			System.out.println("Warning: run out of TypingScene to play");
+			return;
+		}
 		
-		if(primaryStage.getScene() == null) {
+		// Get current TypingScene
+		TypingScene thisTypingScene = allTypingScenes[++currentTypingSceneIndex];
+		
+		if(currentTypingSceneIndex == 0) {
 			// First time, init scene
-			Scene scene = new Scene(typingScene.init_scene(screenWidth, screenHeight), screenWidth, screenHeight, Color.WHITE);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// Init scene
+			Scene scene = new Scene(thisTypingScene.init_scene(screenWidth, screenHeight), screenWidth, screenHeight, Color.WHITE);
 	        primaryStage.setScene(scene);
-	       
+	        
 	        // Listen to key
 	        listenToKey(scene);
 	        
@@ -53,22 +76,34 @@ public class GameManager {
 	        primaryStage.show();
 		} else {
 			// Not first time, only change the root of the scene
-			primaryStage.getScene().setRoot(typingScene.init_scene(screenWidth, screenHeight));
+			primaryStage.getScene().setRoot(thisTypingScene.init_scene(screenWidth, screenHeight));
 		}
 		
         
         // Play scene text
-        typingScene.playText(scrollSpeed);
+        if(thisTypingScene.playText(scrollSpeed) == false) showNextTypingScene();
 	}
 	
 	private void listenToKey(Scene scene) {
-		scene.addEventFilter(KeyEvent.KEY_PRESSED, event->{
-            if (event.getCode() == KeyCode.SPACE) {
-            	if(currentTypingScene !=null && currentTypingScene.playText(scrollSpeed) == false) {
-            		System.out.println("Warning: run out of TypingScene to play");
-                	event.consume();
+		scene.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.SPACE) spaceKeyPressed = true;
+        });
+		
+        scene.setOnKeyReleased(event -> {
+        	if(event.getCode() == KeyCode.SPACE) spaceKeyPressed = false;
+        });
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if(spaceKeyPressed) {
+                    spaceKeyPressed = false; // So no longer trigger multiple times when only pressed once
+ 
+                    if(allTypingScenes[currentTypingSceneIndex].playText(scrollSpeed) == false) {
+                    	showNextTypingScene();
+                    }
                 }
             }
-        });
+        }.start();
 	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -4,36 +4,17 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	final private int screenWidth = 640, screenHeight = 360;
-	
+
 	@Override
 	public void start(Stage primaryStage) {
-		GameManager gameManager = new GameManager(primaryStage);
-		
-		gameManager.setScreenSize(screenWidth, screenHeight);
-		gameManager.setScrollSpeed(1);
-		gameManager.startGame(); // Probably change it to show the game menu instead
+		Menu mainMenu = new Menu();
+
+		mainMenu.initMenu(primaryStage, screenWidth, screenHeight, mainMenu.mMenu(primaryStage));
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
-		
-		//Use this section to test player methods
-		/*Player me = new Player();
-		Player me2 = new Player("Coding Wizard");
-		
-		System.out.println(me.getPlayerInfo());
-		System.out.println(me2.getPlayerInfo());
-		
-		me.adjCha(12);
-		me.adjInt(15);
-		me.adjScore(1337);
-		me.adjHP(-70);
-		
-		me2.adjMP(1000);
-		
-		System.out.println(me.getPlayerInfo());
-		System.out.println(me2.getPlayerInfo());*/
 	}
 }
