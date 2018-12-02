2c72ae991861f54cec0481b3f86e91f34bbc5ad4
danielcodin <daniel@danielcoding.com>
2018-12-01 19:25:00 -0800

    Add two unit test  and one test suite
    1. Unit test GameManger::setPlayerInitialStats
    2. Unit test GameManger::loadPlayerStats

---
+++ src/test/TestGameManagerLoadPlayerStats.java
@@ -0,0 +1,72 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import logic.GameManager;
+import logic.Player;
+
+public class TestGameManagerLoadPlayerStats {
+
+	@Test
+	public void testLoadPlayerStats_HP() {
+		// Initialize player
+		Player player = new Player("Coding Wizard");
+		
+		// Initialize GameManager
+		GameManager gameManager = new GameManager(null);
+		gameManager.loadPlayerStats(player);
+		
+		// Get player
+		Player thisPlayer = gameManager.player;
+		
+		assertEquals(Integer.valueOf(100), thisPlayer.getHP());
+	}
+	
+	@Test
+	public void testLoadPlayerStats_MP() {
+		// Initialize player
+		Player player = new Player("Coding Wizard");
+		
+		// Initialize GameManager
+		GameManager gameManager = new GameManager(null);
+		gameManager.loadPlayerStats(player);
+		
+		// Get player
+		Player thisPlayer = gameManager.player;
+		
+		assertEquals(Integer.valueOf(100), thisPlayer.getMP());
+	}
+	
+	@Test
+	public void testLoadPlayerStats_Int() {
+		// Initialize player
+		Player player = new Player("Coding Wizard");
+		
+		// Initialize GameManager
+		GameManager gameManager = new GameManager(null);
+		gameManager.loadPlayerStats(player);
+		
+		// Get player
+		Player thisPlayer = gameManager.player;
+		
+		assertEquals(Integer.valueOf(5), thisPlayer.getInt());
+	}
+	
+	@Test
+	public void testLoadPlayerStats_Char() {
+		// Initialize player
+		Player player = new Player("Coding Wizard");
+		
+		// Initialize GameManager
+		GameManager gameManager = new GameManager(null);
+		gameManager.loadPlayerStats(player);
+		
+		// Get player
+		Player thisPlayer = gameManager.player;
+		
+		assertEquals(Integer.valueOf(1), thisPlayer.getChar());
+	}
+
+}
