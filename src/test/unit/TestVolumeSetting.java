16f4fb81c8f1bf00348f7de1f060612a1c0174a6
cwjung479 <35707942+cwjung479@users.noreply.github.com>
2018-12-03 23:08:25 -0800

    Add files via upload

---
+++ src/test/unit/TestVolumeSetting.java
@@ -0,0 +1,41 @@
+package test.unit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+import logic.GameManager;
+import logic.MainEvents;
+import logic.Menu;
+
+public class TestVolumeSetting {
+
+	@Test
+	public void testSettingInitialVolume() {
+		Menu menu = new Menu();
+		int originalVolume = menu.volume;
+
+		GameManager gameManager = new GameManager(null);
+		MainEvents main = new MainEvents();
+		main.initMainEvents(gameManager);
+
+		int setVolume = main.getMainEventsArrayList().get(0).setVolumeTest(originalVolume);
+
+		assertEquals(50, setVolume);
+	}
+
+	@Test
+	public void testSettingChangedVolume() {
+		Menu menu = new Menu();
+		menu.volume = 59;
+		int originalVolume = menu.volume;
+
+		GameManager gameManager = new GameManager(null);
+		MainEvents main = new MainEvents();
+		main.initMainEvents(gameManager);
+
+		int setVolume = main.getMainEventsArrayList().get(0).setVolumeTest(originalVolume);
+
+		assertEquals(59, setVolume);
+	}
+
+}
