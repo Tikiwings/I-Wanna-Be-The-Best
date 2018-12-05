f1cd854c4b3308de4d3879d56dd0468e7cae4e3a
cwjung479 <35707942+cwjung479@users.noreply.github.com>
2018-12-04 19:10:41 -0800

    Add files via upload

---
+++ src/test/unit/VolumeAndSoundTestSuite.java
@@ -0,0 +1,17 @@
+// Written by: Chanye Jung
+
+package test.testsuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import test.unit.TestImageSettings;
+import test.unit.TestVolumeSetting;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestImageSettings.class,
+				TestVolumeSetting.class})
+public class VolumeAndSoundTestSuite {
+
+}
